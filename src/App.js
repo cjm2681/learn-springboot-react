25577705e9b035c3fee64962ca60aa1a04c88b63
cjm2681 <cjm2681@naver.com>
2024-09-02 18:28:56 +0900

    react학습

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import LearningComponent from './components/learning-examples/LearningComponent';
+
+
+function App() {
+  return (
+    <div className="App">
+      <LearningComponent/>
+    </div>
+  );
+}
+
+
+
+export default App;
