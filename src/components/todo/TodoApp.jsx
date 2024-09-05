0cc32dd7b2654082d925328d77eea8e5daf0f956
cjm2681 <cjm2681@naver.com>
2024-09-05 15:44:31 +0900

    리팩토링, 인증,createContext 사용으로 컴포넌트간 state 공유

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,184 +1,59 @@
-import { useState } from 'react'
-import { BrowserRouter, Routes, Route, useNavigate, useParams, Link } from 'react-router-dom'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom'
 import './TodoApp.css'
+import LogoutComponent from './LogoutComponent'
+import HeaderComponent from './HeaderComponent'
+import ListTodosComponent from './ListTodosComponent'
+import ErrorComponent from './ErrorComponent'
+import WelcomeComponent from './WelcomeComponent'
+import LoginComponent from './LoginComponent'
+import AuthProvider, { useAuth } from './security/AuthContext'
 
+
+function AuthenticatedRoute({ children }) {
+
+    const authContext = useAuth()
+
+    if (authContext.isAuthenticated) {
+
+        return children
+    }
+
+
+    return <Navigate to="/" />
+}
 
 export default function TodoApp() {
     return (
         <div className="TodoApp">
-            <HeaderComponent/>
-            <BrowserRouter>
-                <Routes>
-                    <Route path='/' element={<LoginComponent />} />
-                    <Route path='/login' element={<LoginComponent />} />
-                    <Route path='/welcome/:username' element={<WelcomeComponent />} />
-                    <Route path='/todos' element={<ListTodosComponent />} />
-                    <Route path='/logout' element={<LogoutComponent />} />
-                    <Route path='*' element={<ErrorComponent />} />
-                </Routes>
-            </BrowserRouter>
-            <FooterComponent/>
-        </div>
-    )
-}
+            <AuthProvider>
+                <BrowserRouter>
+                    <HeaderComponent />
+                    <Routes>
+                        <Route path='/' element={<LoginComponent />} />
+                        <Route path='/login' element={<LoginComponent />} />
 
-function LoginComponent() {
-
-    const [username, setUsername] = useState('in28minutes')
-    const [password, setPassword] = useState('')
-    const [showSuccessMessage, setShowSuccessMessage] = useState(false)
-    const [showErrorMessage, setShowErrorMessage] = useState(false)
-    const navigate = useNavigate()
-
-
-    function handleUsernameChange(event) {
-        setUsername(event.target.value)
-    }
-
-    function handlePasswordChange(event) {
-        setPassword(event.target.value)
-    }
-
-    function handleSubmit() {
-        if (username === "in28minutes" && password === "123") {
-            setShowSuccessMessage(true)
-            setShowErrorMessage(false)
-            navigate(`/welcome/${username}`)
-        } else {
-            setShowSuccessMessage(false)
-            setShowErrorMessage(true)
-        }
-    }
-
-
-    return (
-        <div className="Login">
-            <h1>Time to Login!</h1>
-
-            {showSuccessMessage && <div className="succesMessage">Authenticated Successfully</div>}
-            {showErrorMessage && <div className="errorMessage">Authenticated failed. Please check your credentials.</div>}
-            <div className="LoginForm">
-                <div>
-                    <label>User Name</label>
-                    <input type="text" name="username" value={username} onChange={handleUsernameChange} />
-                </div>
-                <div>
-                    <label>Password</label>
-                    <input type="password" name="password" value={password} onChange={handlePasswordChange} />
-                </div>
-                <div>
-                    <button type="button" name="login" onClick={handleSubmit}>login</button>
-                </div>
-            </div>
-
-
+                        <Route path='/welcome/:username' element={
+                            <AuthenticatedRoute>
+                                <WelcomeComponent />
+                            </AuthenticatedRoute>
+                        } />
+                        <Route path='/todos' element={
+                            <AuthenticatedRoute>
+                                <ListTodosComponent />
+                            </AuthenticatedRoute>
+                        } />
+                        <Route path='/logout' element={
+                            <AuthenticatedRoute>
+                                <LogoutComponent />
+                            </AuthenticatedRoute>
+                        } />
+                        <Route path='*' element={<ErrorComponent />} />
+                    </Routes>
+                </BrowserRouter>
+            </AuthProvider>
         </div>
     )
 }
 
 
-
-function WelcomeComponent() {
-
-    const { username } = useParams()
-
-    console.log(username)
-
-    return (
-        <div className="WelcomeComponent">
-            <h1>Welcome {username}</h1>
-            <div>
-                Manage your todos - <Link to="/todos">Go here</Link>
-            </div>
-        </div>
-
-    )
-}
-
-function ErrorComponent() {
-    return (
-        <div className="ErrorComponent">
-            <h1>We are working really hard!</h1>
-            <div>
-                Apologies for the 404. Reach out to our team at ABC.
-            </div>
-        </div>
-    )
-}
-
-function ListTodosComponent() {
-
-    const today = new Date();
-    const targetDate = new Date(today.getFullYear() + 12, today.getMonth(), today.getDay())
-
-    const todos = [
-        { id: 1, description: 'Learn AWS', done: false, targetDate: targetDate },
-        { id: 2, description: 'Learn Full Stack Dev', done: false, targetDate: targetDate },
-        { id: 3, description: 'Learn DevOps', done: false, targetDate: targetDate },
-    ]
-
-    return (
-        <div className="container">
-            <h1>Things You Want To Do!</h1>
-            <div>
-                <table className="table">
-                    <thead>
-                        <tr>
-                            <td>Id</td>
-                            <td>Description</td>
-                            <td>Is Done?</td>
-                            <td>Target Date</td>
-                        </tr>
-                    </thead>
-
-                    <tbody>
-                        {
-                            todos.map(
-                                todo => (
-                                    <tr key={todo.id}>
-                                        <td>{todo.id}</td>
-                                        <td>{todo.description}</td>
-                                        <td>{todo.done.toString()}</td>
-                                        <td>{todo.targetDate.toDateString()}</td>
-                                    </tr>
-                                )
-                            )
-                        }
-
-                    </tbody>
-
-                </table>
-            </div>
-        </div>
-    )
-}
-
-
-
-function HeaderComponent() {
-    return (
-        <div className="header">
-            Header <hr/>
-        </div>
-    )
-}
-
-function FooterComponent() {
-    return (
-        <div className="footer">
-             <hr/> Header
-        </div>
-    )
-}
-
-function LogoutComponent() {
-    return (
-        <div className="LogoutComponent">
-            <h1>You are logged out!</h1>
-            <div>
-                Thank you for useing our App. Come back soon!
-            </div>
-        </div>
-    )
-}
 
