f4b4cbfcdcce2e1607fff577be5513696d7ebc3b
cjm2681 <cjm2681@naver.com>
2024-09-09 21:48:14 +0900

    todoServer

--- 01-rest-api-starting-code/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
+++ 01-rest-api-starting-code/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/RestfulWebServicesApplication.java
@@ -2,6 +2,7 @@ package com.in28minutes.rest.webservices.restfulwebservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -13,16 +14,16 @@ public class RestfulWebServicesApplication {
 		SpringApplication.run(RestfulWebServicesApplication.class, args);
 	}
 
-	@CrossOrigin
+	@CrossOrigin(origins = "http://localhost:3000", allowCredentials = "true")
+	@Bean
 	public WebMvcConfigurer corsConfigurer() {
 		return new WebMvcConfigurer() {
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-					.allowedMethods("*")
-					.allowedOrigins("Origin': 'http://localhost:3000");
+					.allowedMethods("*");
 			}
 			
 			
 		};
-	}
+	}  
 }
