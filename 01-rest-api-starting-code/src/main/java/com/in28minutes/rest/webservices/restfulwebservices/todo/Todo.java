24da5bffaa5e71e48ef31a689f0f5897d690bf5e
cjm2681 <cjm2681@naver.com>
2024-09-13 21:35:12 +0900

    sql삽입연습

--- 01-rest-api-starting-code/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/Todo.java
+++ 01-rest-api-starting-code/src/main/java/com/in28minutes/rest/webservices/restfulwebservices/todo/Todo.java
@@ -2,6 +2,12 @@ package com.in28minutes.rest.webservices.restfulwebservices.todo;
 
 import java.time.LocalDate;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+
+@Entity
 public class Todo {
 
 	public Todo() {
@@ -17,6 +23,8 @@ public class Todo {
 		this.done = done;
 	}
 
+	@Id
+	@GeneratedValue
 	private int id;
 
 	private String username;
