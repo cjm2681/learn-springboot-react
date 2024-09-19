741ec958bdcdf48b474791b2c7856465b6bfeb42
cjm2681 <cjm2681@naver.com>
2024-09-19 17:56:50 +0900

    junit5 사용법공부

---
+++ junit-in-5-steps/test/com/in28minutes/junit/MyBeforeAfterTest.java
@@ -0,0 +1,52 @@
+package com.in28minutes.junit;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MyBeforeAfterTest {
+
+	@BeforeAll
+	static void beforeAll() {
+		System.out.println("BeforeAll");
+
+	}
+	
+	@BeforeEach
+	void beforeEach() {
+		System.out.println("BeforeEach");
+
+	}
+	
+	@Test
+	void test() {
+		System.out.println("test1");
+	}
+	
+	@Test
+	void test2() {
+		System.out.println("test2");
+	}
+
+	
+	@Test
+	void test3() {
+		System.out.println("test3");
+	}
+	
+	@AfterEach
+	void AfterEach() {
+		System.out.println("AfterEach");
+
+	}
+	
+	@AfterAll
+	static void AfterAll() {
+		System.out.println("AfterAll");
+
+	}
+
+
+}
